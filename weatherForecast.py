f13c6fe6035411b1f87e55aa13fb58c0cd242e3c
cmolarti <cmolarti@gmail.com>
2023-12-21 22:51:41 -0500

    create a dynamic plotly line plot in streamlit

--- weatherForecast.py
+++ weatherForecast.py
@@ -1,5 +1,6 @@
 import streamlit as st
 import pandas as pd
+import plotly.express as px
 
 st.title('Weather Forecast for the Next Days')
 
@@ -10,3 +11,15 @@ days = st.slider(label='Days', min_value=1, max_value=5, step=1, key='slider', h
 option = st.selectbox(label='Select data to view', options=['Temperature', 'Sky'], key='selectbox')
 
 st.header(f'{option} for the next {days} days in {place.title()}')
+
+
+def get_data(days):
+    dates = ['2022-10-25', '2022-10-26', '2022-10-27', '2022-10-28', '2022-10-29']
+    # temp = [(-4, 8), (0, 10), (1,11), (3,15), (-2, 7)]
+    temp = [5, 7, 9, 12, 11]
+    return dates[0:days], temp[0:days]
+
+dates, temp = get_data(days)
+
+fig = px.line(x=dates, y=temp, labels={'x':'Dates', 'y':'Temp (C)'})
+st.plotly_chart(fig)
